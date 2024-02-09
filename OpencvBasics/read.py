58a742ce23789dd38532e217dbeb440c7e6fc27e
CK-EmbPro <kennydebrice2@gmail.com>
2024-02-09 10:58:15 +0200

    How to write the texts in videos while playing

--- OpencvBasics/read.py
+++ OpencvBasics/read.py
@@ -9,6 +9,7 @@ import cv2 as cv
 capture = cv.VideoCapture('videos/dog.mp4')
 while True:
     isTrue, frame = capture.read()
+    cv.putText(frame, "hello dog", (0,200), cv.FONT_HERSHEY_TRIPLEX, 1.0, (255,0,0), 1)
     cv.imshow('Video', frame)
 
     if cv.waitKey(20) & 0xFF == ord('d'):
